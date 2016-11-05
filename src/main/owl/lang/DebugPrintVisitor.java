01f0a37132820aeca9755b2b27fd18c85e44005f
Igor Demura <igor.demura@gmail.com>
2016-11-04 22:41:19 -0700

    Remove TabPrinter

--- src/main/owl/lang/DebugPrintVisitor.java
+++ src/main/owl/lang/DebugPrintVisitor.java
@@ -1,7 +1,37 @@
 package owl.lang;
 
+
+final class DebugPrinter {
+    private int tab = 0;
+    private boolean newLine = true;
+
+    void print(String s) {
+        int t = tab;
+        if (s.endsWith("{\n")) {
+            tab++;
+            s = s.substring(0, s.length() - 2) + "\n";
+        }
+        if (s.endsWith("}\n")) {
+            tab--;
+            if (s.length() == 2) {
+                newLine = true;
+                return;
+            }
+            s = s.substring(0, s.length() - 2) + "\n";
+        }
+        if (newLine) {
+            for (int i = 0; i < t; i++) {
+                System.out.print("  ");
+            }
+        }
+        newLine = s.endsWith("\n");
+        System.out.print(s);
+    }
+}
+
+
 class DebugPrintVisitor implements AstNode.Visitor {
-    private TabPrinter printer = new TabPrinter();
+    private DebugPrinter printer = new DebugPrinter();
 
     @Override
     public void visit(AstName n) {
